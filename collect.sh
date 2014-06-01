75a328e2daf299e0e7070aa552abbf828d3e3652
David Cook <divergentdave@gmail.com>
2014-06-01 13:49:15 -0500

    Initial commit, includes Upside-Downter-Net and tor bridge

---
+++ collect.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cp /etc/hostapd/hostapd.conf .
+cp /etc/dhcp/dhcpd.conf .
+cp /etc/default/isc-dhcp-server .
+cp /etc/network/interfaces .
+cp /etc/sysctl.conf .
+cp /etc/squid/squid.conf .
+cp /etc/apache2/conf.d/myapache.local.conf .
+cp /etc/tor/torrc .
+cp /etc/crontab .
