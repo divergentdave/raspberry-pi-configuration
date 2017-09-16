dac86465f5341244d580a2c9c98e4129a42666bb
David Cook <divergentdave@gmail.com>
2017-09-15 20:11:45 -0500

    Add Leiningen plugins

--- profiles.clj
+++ profiles.clj
@@ -1,2 +1,6 @@
 {:user {:dependencies [[cljfmt "0.5.6"]]
+        :plugins      [[lein-cljfmt "0.5.6"]
+                       [lein-ancient "0.6.10"]
+                       [lein-kibit "0.1.4-SNAPSHOT"]
+                       [jonase/eastwood "0.2.4"]]
         :repl-options {:timeout 300000}}}
